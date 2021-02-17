34e21ef20c801076322662500ad084b10175e893
hdnkt <69378772+hdnkt@users.noreply.github.com>
2021-02-17 16:50:06 +0900

    Demo

--- hello/views.py
+++ hello/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 import os
+import json
+from bs4 import BeautifulSoup
 
 from .models import Greeting
+
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
 
     #レーティンググラフ
-    #return render(request, "ratinggraph.html")
+    return render(request, "ratinggraph.html")
 
     #ティーポット
     #r = requests.get('http://httpbin.org/status/418')
@@ -18,12 +21,20 @@ def index(request):
     #return HttpResponse('<pre>' + r.text + '</pre>')
 
     #
-    return render(request,"form.html")
+    #return render(request,"form.html")
 
 def vote(request):
     tmp = request.POST["username"]
+    #該当ユーザーの履歴をjsonでゲット
+    #r = requests.get('https://atcoder.jp/users/'+tmp+'/history/json').json()
 
-    return HttpResponse('<pre>' + tmp + '</pre>')
+    #すくれいびんぐ
+    site=requests.get("https://atcoder.jp/users/"+tmp)
+    data = BeautifulSoup(site.text,"html.parser")
+
+    ret = data.find_all("script")
+
+    return HttpResponse('<pre>'+str(ret[12])[27:-10]+'</pre>')
 
 
 def db(request):
